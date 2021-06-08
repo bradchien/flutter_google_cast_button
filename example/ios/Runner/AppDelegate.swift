1a49907ba50ffdb287e31afe751a44faeb06016b
Brad Chien <bradchien@kktv.me>
2021-06-08 16:23:29 +0800

    fix example crash

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -4,20 +4,17 @@ import GoogleCast
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+    let kReceiverAppID = kGCKDefaultMediaReceiverApplicationID
+
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let criteria = GCKDiscoveryCriteria(applicationID: kReceiverAppID)
+    let options = GCKCastOptions(discoveryCriteria: criteria)
+    GCKCastContext.setSharedInstanceWith(options)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    override func applicationDidFinishLaunching(_ application: UIApplication) {
-        // TODO replace kReceiverAppID as yours
-        let criteria = GCKDiscoveryCriteria(applicationID: "kReceiverAppID")
-        let options = GCKCastOptions(discoveryCriteria: criteria)
-        GCKCastContext.setSharedInstanceWith(options)
-        
-        return super.applicationDidFinishLaunching(application)
-    }
 }
